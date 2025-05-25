9af5bd20ec06ff9879c585d3c972f55dad821f59
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2025-05-25 10:52:05 +0600

    fix-code

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,6 +6,7 @@ import logo2 from '/public/logo/logo2.png';
 import Image from 'next/image';
 import Link from 'next/link';
 import { SiShopify } from 'react-icons/si';
+import { HiMenuAlt1 } from 'react-icons/hi';
 
 
 const Header = () => {
@@ -15,10 +16,14 @@ const Header = () => {
     <MaxWidthWrapper>
 
         <div className='flex justify-between gap-3 items-center   h-20'>
+             <div className=''> 
+                <button className='text-black cursor-pointer'><HiMenuAlt1   size={40} /></button>
+           
+            </div>
             <Link href="#">
                 <Image src={logo2} width={100} height={100} alt='logo' className='w-[100px] h-[80px]'/>
             </Link>
-            <div className='text-2xl font-semibold  text-black'>Your Brand </div>
+    
             <div className='relative'> 
                 <button className='text-black cursor-pointer'><SiShopify  size={40} /></button>
                 <span className='text-xs font-normal bg-[#222831] text-white p-0.5 rounded-full absolute -top-2 -right-1'>02</span>
